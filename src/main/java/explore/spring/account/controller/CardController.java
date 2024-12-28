3e1c09fc7d564b6e996b44593a20f36551e87999
alfonsussetiawanjacub <out2240@astrapay.com>
2024-12-28 13:35:10 +0700

    feat(loan-service) : test

--- src/main/java/explore/spring/account/controller/CardController.java
+++ src/main/java/explore/spring/account/controller/CardController.java
@@ -22,62 +22,6 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "CRUD REST APIs for Cards", description = "CREATE, READ, UPDATE, DELETE")
 public class CardController {
 
-    private final CardService cardService;
-
-    @Operation(summary = "Create Cards")
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    ResponseDto<Void> createCards(
-            @RequestParam
-            @CardNotRegistered
-            @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
-            String mobileNumber
-    ){
-        cardService.createCards(mobileNumber);
-        return ResponseDto.<Void>builder()
-                .statusCode(HttpStatus.CREATED.value())
-                .statusMsg(StatusMessage.SUCCESS_CREATE_CARDS)
-                .build();
-    }
-
-    @Operation(summary = "Update Cards")
-    @PutMapping("/{cardNumber}")
-    @ResponseStatus(HttpStatus.OK)
-    ResponseDto<Void> updateCards(
-            @PathVariable @CardExist(validateBy = CardExist.CardExistEnum.CARD_NUMBER) String cardNumber,
-            @RequestBody CardDto cardDto
-    ){
-        cardService.updateCard(cardNumber, cardDto);
-        return ResponseDto.<Void>builder()
-                .statusCode(HttpStatus.OK.value())
-                .statusMsg(StatusMessage.SUCCESS_UPDATED_CARDS)
-                .build();
-    }
-
-    @Operation(summary = "Get Detail Cards")
-    @GetMapping("/{cardNumber}")
-    ResponseDto<CardResponseDto> getDetailCards(
-            @PathVariable @CardExist(validateBy = CardExist.CardExistEnum.CARD_NUMBER) String cardNumber
-    ){
-        return ResponseDto.<CardResponseDto>builder()
-                .data(cardService.getDetailCard(cardNumber))
-                .statusCode(HttpStatus.OK.value())
-                .statusMsg(StatusMessage.SUCCESS_GET_CARDS)
-                .build();
-    }
-
-
-    @Operation(summary = "Delete Cards")
-    @DeleteMapping("/{cardNumber}")
-    ResponseDto<Void> deleteCards(
-            @PathVariable @CardExist(validateBy = CardExist.CardExistEnum.CARD_NUMBER) String cardNumber
-    ){
-        cardService.deleteCard(cardNumber);
-        return ResponseDto.<Void>builder()
-                .statusCode(HttpStatus.OK.value())
-                .statusMsg(StatusMessage.SUCCESS_DELETE_CARDS)
-                .build();
-    }
 
 
 }
